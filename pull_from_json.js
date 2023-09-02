ca883c3590605b04afc99526225d28a297b2df7a
Ram Goel <ramgoel@rams-air.lan>
2023-09-01 19:51:08 -0700

    random entry

--- pull_from_json.js
+++ pull_from_json.js
@@ -1,22 +1,22 @@
 // Function to fetch and display a random HTML entry
-        async function displayRandomEntry() {
-            try {
-                // Fetch the JSON data from the file
-                const response = await fetch('data.json');
-                const data = await response.json();
+async function displayRandomEntry() {
+    try {
+        // Fetch the JSON data from the file
+        const response = await fetch('JSON_AMC_files/2017_AMC10A.json');
+        const data = await response.json();
 
-                // Generate a random index to select a random entry
-                const randomIndex = Math.floor(Math.random() * data.length);
-                const randomEntry = data[randomIndex];
+        // Generate a random index to select a random entry
+        const randomIndex = Math.floor(Math.random() * data.length);
+        const randomEntry = data[randomIndex];
 
-                // Display the HTML content
-                const contentContainer = document.getElementById('content-container');
-                contentContainer.innerHTML = randomEntry.htmlContent;
-            } catch (error) {
-                console.error('Error fetching or displaying data:', error);
-            }
-        }
+        // Display the HTML content
+        const contentContainer = document.getElementById('content-container');
+        contentContainer.innerHTML = randomEntry.htmlContent;
+    } catch (error) {
+        console.error('Error fetching or displaying data:', error);
+    }
+}
 
-        // Add an event listener to the button to trigger the display
-        const randomButton = document.getElementById('random-button');
-        randomButton.addEventListener('click', displayRandomEntry);
+// Add an event listener to the button to trigger the display
+const randomButton = document.getElementById('random-button');
+randomButton.addEventListener('click', displayRandomEntry);
